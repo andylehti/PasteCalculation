806c2683467791dcedbd0059b050d56eeb6c7935
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2023-07-18 01:45:01 -0500

    Update PasteCalc.py

--- PasteCalc.py
+++ PasteCalc.py
@@ -1,6 +1,89 @@
 import mpmath
+import re
+import math
 
-# Paste values between the third line
+def calculate_result(v, number, operation, precision, output_option):
+    mpmath.mp.dps = 1000 + precision
+
+    lines = v.split('\n')
+
+    array = []
+    for line in lines:
+        values = re.findall(r'\d+', line)
+        if values:
+            array.extend(map(int, values))
+
+    if number == 0:
+        total_result = mpmath.mpf(array.pop(0))
+    else:
+        total_result = mpmath.mpf(number)
+
+    result_strings = []  # To store the equations of each iteration
+
+    for i, value in enumerate(array):
+        last_result = total_result
+
+        if operation == 0:
+            total_result += value
+            operation_symbol = '+'
+        elif operation == 1:
+            total_result -= value
+            operation_symbol = '-'
+        elif operation == 2:
+            total_result *= value
+            operation_symbol = '⋅'
+        elif operation == 3:
+            total_result /= value
+            operation_symbol = '/'
+        else:
+            raise ValueError("Invalid operation. Please choose 0 for 'add', 1 for 'subtract', 2 for 'multiply', or 3 for 'divide'.")
+
+        if output_option[0] == -1:
+            output_result = f"{mpmath.floor(total_result)}"
+        elif output_option[0] == 0:
+            output_result = f"{mpmath.nstr(total_result, 10, min_fixed=0, max_fixed=0)}"
+        elif output_option[0] == 1:
+            output_result = f"{math.ceil(total_result)}"
+        elif output_option[0] == 2:
+            output_result = f"{total_result}"
+        elif output_option[0] == 3:
+            output_result = f"{str(total_result)[:101]}"
+
+        if output_option[3]:  # show_equation is True
+            result_string = f"{last_result} {operation_symbol} {value} = {output_result}"
+        else:
+            result_string = f"{output_result}"
+
+        result_strings.append(result_string)
+
+        if output_option[2]:  # print_iterations is True
+            print(result_string)
+
+    # Final output result
+    if output_option[0] == -1:
+        final_output = f"The full rounded down result is: {mpmath.floor(total_result)}"
+    elif output_option[0] == 0:
+        final_output = f"The result in scientific notation is: {mpmath.nstr(total_result, 10, min_fixed=0, max_fixed=0)}"
+    elif output_option[0] == 1:
+        final_output = f"The full rounded up result is: {math.ceil(total_result)}"
+    elif output_option[0] == 2:
+        final_output = f"The full result is: {total_result}"
+    elif output_option[0] == 3:
+        final_output = f"The truncated result is: {str(total_result)[:101]}"
+
+    print(final_output)
+
+    if output_option[1]:  # save final result to file
+        with open("calcd.txt", 'w') as f:
+            f.write(final_output)
+        print("The final result has been saved to 'calcd.txt'.")
+
+    if output_option[2]:  # save all outputs to file
+        with open("calcd_all.txt", 'w') as f:
+            f.write('\n'.join(result_strings))
+            f.write('\n' + final_output)
+        print("The iterations and final result have been saved to 'calcd_all.txt'.")
+
 v = '''
 199999999999999
 19999999999999
@@ -17,54 +100,11 @@ v = '''
 199
 19
 1
-'''  # and here
+'''
 
-# outputs the above if the below is 0, 2, 15, False, 1: 30956745117962895292825159691675032847559175394706377094119397647307187439784996092023295962076506738837243562
+number = 0
+operation = 0
+precision = 15
+output_option = (1, True, True, True)
 
-number = 0  # addition, subtraction, multiplication and division will add to, subtract from, multiply by, and divide by each in array respectively
-operation = 2  # operation: 0 = "add", 1 = "subtract", 2 = "multiply", 3 = "divide"
-precision = 15  # dynamic precision (adjust as needed)
-print_iterations = False  # option to print result after each iteration
-output_option = 1  # output options: 0 = partial scientific, 1 = full, 2 = save to file, 3 = partial truncated
-
-mpmath.mp.dps = 1000 + precision
-values = v.strip().split('\n')
-array = list(map(int, values))
-
-if number == 0:
-    total_result = mpmath.mpf(array.pop(0))
-else:
-    total_result = mpmath.mpf(number)
-
-for i, value in enumerate(array):
-
-    if operation == 0:
-        total_result += value
-    elif operation == 1:
-        total_result -= value
-    elif operation == 2:
-        total_result *= value
-    elif operation == 3:
-        total_result /= value
-    else:
-        raise ValueError("Invalid operation. Please choose 0 for 'add', 1 for 'subtract', 2 for 'multiply', or 3 for 'divide'.")
-
-    if print_iterations:
-        print(f"Iteration {i+1}: {total_result}")
-
-output_options = {
-    0: f"The result in scientific notation is: {mpmath.nstr(total_result, 10, min_fixed=0, max_fixed=0)}",
-    1: f"The full result is: {total_result}",
-    2: "calcd.txt",
-    3: f"The truncated result is: {str(total_result)[:101]}"
-}
-
-if output_option in output_options:
-    if output_option == 2:
-        with open(output_options[output_option], 'w') as f:
-            f.write(str(total_result))
-        print("The result has been saved to 'calcd.txt'.")
-    else:
-        print(output_options[output_option])
-else:
-    raise ValueError("Invalid output option. Please choose between 0, 1, 2, and 3.")
+calculate_result(v, number, operation, precision, output_option)
